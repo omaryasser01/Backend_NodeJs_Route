4b40d33cab4c461c170303f29a12a0d29b59b183
omaryasser01 <omaryasser5455@gmail.com>
2025-12-13 21:38:08 +0200

    finalll

---
+++ assignment_2.js
@@ -0,0 +1,5 @@
+// 1. Write a function that logs the current file path and directory. (0.5 Grade)
+// • Output Example:{File:“/home/user/project/index.js”, Dir:“/home/user/project”}
+// console.log(__filedir);
+console.log("AAAAA");
+console.log("AAAAA");
--- test.js
+++
@@ -1,5 +0,0 @@
-//test
-jjkjkj
-klokjjk
-
-kkjkj
